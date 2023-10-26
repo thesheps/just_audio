4296227b835a9507c7429c230e9ddaa0d7547f3c
Sheps <thesheps@gmail.com>
2023-10-26 08:30:48 +0100

    bug: updates example to demonstrate iOS setClip crash

--- just_audio/example/lib/main.dart
+++ just_audio/example/lib/main.dart
@@ -123,85 +123,125 @@ class ControlButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
+    return Column(
       children: [
-        // Opens volume slider dialog
-        IconButton(
-          icon: const Icon(Icons.volume_up),
-          onPressed: () {
-            showSliderDialog(
-              context: context,
-              title: "Adjust volume",
-              divisions: 10,
-              min: 0.0,
-              max: 1.0,
-              value: player.volume,
-              stream: player.volumeStream,
-              onChanged: player.setVolume,
-            );
-          },
-        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Opens volume slider dialog
+            IconButton(
+              icon: const Icon(Icons.volume_up),
+              onPressed: () {
+                showSliderDialog(
+                  context: context,
+                  title: "Adjust volume",
+                  divisions: 10,
+                  min: 0.0,
+                  max: 1.0,
+                  value: player.volume,
+                  stream: player.volumeStream,
+                  onChanged: player.setVolume,
+                );
+              },
+            ),
 
-        /// This StreamBuilder rebuilds whenever the player state changes, which
-        /// includes the playing/paused state and also the
-        /// loading/buffering/ready state. Depending on the state we show the
-        /// appropriate button or loading indicator.
-        StreamBuilder<PlayerState>(
-          stream: player.playerStateStream,
-          builder: (context, snapshot) {
-            final playerState = snapshot.data;
-            final processingState = playerState?.processingState;
-            final playing = playerState?.playing;
-            if (processingState == ProcessingState.loading ||
-                processingState == ProcessingState.buffering) {
-              return Container(
-                margin: const EdgeInsets.all(8.0),
-                width: 64.0,
-                height: 64.0,
-                child: const CircularProgressIndicator(),
-              );
-            } else if (playing != true) {
-              return IconButton(
-                icon: const Icon(Icons.play_arrow),
-                iconSize: 64.0,
-                onPressed: player.play,
-              );
-            } else if (processingState != ProcessingState.completed) {
-              return IconButton(
-                icon: const Icon(Icons.pause),
-                iconSize: 64.0,
-                onPressed: player.pause,
-              );
-            } else {
-              return IconButton(
-                icon: const Icon(Icons.replay),
-                iconSize: 64.0,
-                onPressed: () => player.seek(Duration.zero),
-              );
-            }
-          },
+            /// This StreamBuilder rebuilds whenever the player state changes, which
+            /// includes the playing/paused state and also the
+            /// loading/buffering/ready state. Depending on the state we show the
+            /// appropriate button or loading indicator.
+            StreamBuilder<PlayerState>(
+              stream: player.playerStateStream,
+              builder: (context, snapshot) {
+                final playerState = snapshot.data;
+                final processingState = playerState?.processingState;
+                final playing = playerState?.playing;
+                if (processingState == ProcessingState.loading ||
+                    processingState == ProcessingState.buffering) {
+                  return Container(
+                    margin: const EdgeInsets.all(8.0),
+                    width: 64.0,
+                    height: 64.0,
+                    child: const CircularProgressIndicator(),
+                  );
+                } else if (playing != true) {
+                  return IconButton(
+                    icon: const Icon(Icons.play_arrow),
+                    iconSize: 64.0,
+                    onPressed: player.play,
+                  );
+                } else if (processingState != ProcessingState.completed) {
+                  return IconButton(
+                    icon: const Icon(Icons.pause),
+                    iconSize: 64.0,
+                    onPressed: player.pause,
+                  );
+                } else {
+                  return IconButton(
+                    icon: const Icon(Icons.replay),
+                    iconSize: 64.0,
+                    onPressed: () => player.seek(Duration.zero),
+                  );
+                }
+              },
+            ),
+
+            // Opens speed slider dialog
+            StreamBuilder<double>(
+              stream: player.speedStream,
+              builder: (context, snapshot) => IconButton(
+                icon: Text("${snapshot.data?.toStringAsFixed(1)}x",
+                    style: const TextStyle(fontWeight: FontWeight.bold)),
+                onPressed: () {
+                  showSliderDialog(
+                    context: context,
+                    title: "Adjust speed",
+                    divisions: 10,
+                    min: 0.5,
+                    max: 1.5,
+                    value: player.speed,
+                    stream: player.speedStream,
+                    onChanged: player.setSpeed,
+                  );
+                },
+              ),
+            ),
+          ],
         ),
-        // Opens speed slider dialog
-        StreamBuilder<double>(
-          stream: player.speedStream,
-          builder: (context, snapshot) => IconButton(
-            icon: Text("${snapshot.data?.toStringAsFixed(1)}x",
-                style: const TextStyle(fontWeight: FontWeight.bold)),
-            onPressed: () {
-              showSliderDialog(
-                context: context,
-                title: "Adjust speed",
-                divisions: 10,
-                min: 0.5,
-                max: 1.5,
-                value: player.speed,
-                stream: player.speedStream,
-                onChanged: player.setSpeed,
-              );
-            },
-          ),
-        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              child: const Text('0-30 secs'),
+              onPressed: () async {
+                await player.setClip(
+                  start: Duration.zero,
+                  end: const Duration(seconds: 30),
+                );
+              },
+            ),
+            TextButton(
+              child: const Text('30-60 secs'),
+              onPressed: () async {
+                await player.setClip(
+                  start: const Duration(seconds: 30),
+                  end: const Duration(seconds: 60),
+                );
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.loop),
+              iconSize: 64.0,
+              onPressed: () async {
+                await player.setLoopMode(LoopMode.one);
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.stop),
+              iconSize: 64.0,
+              onPressed: () => player.stop(),
+            )
+          ],
+        )
       ],
     );
   }
